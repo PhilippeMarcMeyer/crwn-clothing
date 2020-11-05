37873d172e09eee2e3701b7313240ef535e534d0
Philippe MEYER <p.meyer@mappingcontrol.com>
2020-11-05 08:54:41 +0100

    header is on ! (unfinished)

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import './collection-item.styles.scss';
 
-const collectionItem = ({id,name,price,imageUrl}) => {
+const collectionItem = ({ id, name, price, imageUrl }) => (
+    <div className='collection-item'>
+        <div className='image' style={{
+            backgroundImage: `url(${imageUrl})`
 
-    return (
-        <div className='collection-item'>
-            <div className='image' style={{
-                backgroundImage : `url(${imageUrl})`
+        }} />
+        <div className="collection-footer">
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
 
-            }}/>
-            <div className="collection-footer">
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-
-            </div>
         </div>
+    </div>
 
-    )
-}
+)
 export default collectionItem;
